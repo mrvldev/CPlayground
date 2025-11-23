8bcfdf85f53059aa28cd220a4a68d188f102b8f8
mrvldev <mikerawvonl@gmail.com>
2025-11-23 11:38:44 +0100

    performed some lessons about functions and return values

---
+++ main.dSYM/ageCheck/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool ageCheck(int age){
+
+    if(age >= 18){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+int main (){
+
+    int age = 12;
+
+    if(ageCheck(age)){
+        printf("You may sign up");
+    }
+    else{
+        printf("You must be 18+ to sign up");
+    }
+
+    return 0;
+}
---
+++ main.dSYM/return/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+double cube(double num){
+    return num * num * num;
+}
+
+double square(double num){
+    return num * num;
+}
+
+int main(){
+
+    // return = returns a value back to where you call a function
+
+    double x = cube(2);
+    double y = cube(3);
+    double z = cube(4);
+
+    printf("%lf\n", x);
+    printf("%lf\n", y);
+    printf("%lf\n", z);
+
+    return 0;
+}
